3351aac3f19bcca2a4f6c9bb4071da3d4c549350
Kushal <kjm8374@rit.edu>
2022-02-15 21:53:32 -0500

    Updated uart.c and uart.h with Lab4 content

--- uart.c
+++ uart.c
@@ -2,13 +2,12 @@
  * File:        uart.c
  * Purpose:     Provide UART routines for serial IO
  *
- * Notes:		Thomas Frumusa
+ * Notes:		
  *
  */
 
 #include "msp.h"
 #include "uart.h"  // you need to create this file with the function prototypes
-#include "Common.h"  // from Lab1 code
 
 #define BAUD_RATE 9600      //default baud rate 
 extern uint32_t SystemCoreClock;  // clock rate of MCU
@@ -16,11 +15,9 @@ extern uint32_t SystemCoreClock;  // clock rate of MCU
 
 void uart0_init()
 {
-	
 	//Set the UART to RESET state (set bit0 of EUSCI_A0->CTLW0 register to '1'
-	
-	
 	EUSCI_A0->CTLW0 |= BIT0;
+	;
 
 	// bit15=0,      no parity bits
 	// bit14=x,      not used when parity is disabled
@@ -37,76 +34,162 @@ void uart0_init()
 	// bit0=1,       hold logic in reset state while configuring
 
 	// set CTLW0 - hold logic and configure clock source to SMCLK
-	// UCSWRST = bit0 ; UCSSEL__SMCLK = bit 6 and 7
-	//UCA0CTLW0 = UCSSEL__SMCLK | UCSWRST;
-	//setting 0x00c1 sets bits 0,6,7
-	EUSCI_A0->CTLW0 |= 0x00C1;
-
+	EUSCI_A0->CTLW0 = 0x80;
+	EUSCI_A0->CTLW0 |= BIT6;
+	EUSCI_A0->CTLW0 |= BIT7;
+	
 	// baud rate
 	// N = clock/baud rate = clock_speed/BAUD_RATE
 	// set BRW register
-	EUSCI_A0->BRW |= SystemCoreClock/BAUD_RATE;
-	
+	EUSCI_A0->BRW = 312;
+
 	 // clear first and second modulation stage bit fields
-	// MCTLW register; 
-	EUSCI_A0->MCTLW &= ~(0xFFF0);
+	// MCTLW register;  
+	EUSCI_A0->MCTLW = 0x00;
 	
+
 	// P1.3 = TxD
 	// P1.2 = RxD
 	// we will be using P1.2, P1.3 for RX and TX but not in I/O mode, so we set Port1 SEL1=0 and SEL0=1
 	// set SEL0, SEL1 appropriately
-
 	P1->SEL0 |= BIT2;
+	P1->SEL1 &= ~BIT2;
 	P1->SEL0 |= BIT3;
-	P1->SEL1 &= ~BIT2;	
-	P1->SEL1 &= ~BIT3;	
-
+	P1->SEL1 &= ~BIT3;
 
 
 	// CTLW0 register - release from reset state
 	EUSCI_A0->CTLW0 &= ~BIT0;
-	//UCA0CTLW0 &= ~UCSWRST;
 
 	// disable interrupts (transmit ready, start received, transmit empty, receive full)	
-	// IE register; 
-	EUSCI_A0->IE |= 0x0;
-	
-
+	// IE register;      
+	EUSCI_A0->IE &= ~BIT0;
+	EUSCI_A0->IE &= ~BIT1;
+	EUSCI_A0->IE &= ~BIT2;
+	EUSCI_A0->IE &= ~BIT3;
 }
 
+void uart2_init()	
+{
+		EUSCI_A2->CTLW0 |= BIT0;
+
+	// set CTLW0 - hold logic and configure clock source to SMCLK
+	EUSCI_A2->CTLW0 = 0x80;
+	EUSCI_A2->CTLW0 |= BIT6;
+	EUSCI_A2->CTLW0 |= BIT7;
+	
+	// baud rate
+	// N = clock/baud rate = clock_speed/BAUD_RATE
+	// set BRW register //9600
+	EUSCI_A2->BRW = 312;
+
+	 // clear first and second modulation stage bit fields
+	// MCTLW register;  
+	EUSCI_A2->MCTLW = 0x00;
+	
+
+	// P1.3 = TxD
+	// P1.2 = RxD
+	// we will be using P1.2, P1.3 for RX and TX but not in I/O mode, so we set Port1 SEL1=0 and SEL0=1
+	// set SEL0, SEL1 appropriately
+	P3->SEL0 |= BIT2;
+	P3->SEL1 &= ~BIT2;
+	P3->SEL0 |= BIT3;
+	P3->SEL1 &= ~BIT3;
+
+
+	// CTLW0 register - release from reset state
+	EUSCI_A2->CTLW0 &= ~BIT0;
+
+	// disable interrupts (transmit ready, start received, transmit empty, receive full)	
+	// IE register;      
+	EUSCI_A2->IE &= ~BIT0;
+	EUSCI_A2->IE &= ~BIT1;
+	EUSCI_A2->IE &= ~BIT2;
+	EUSCI_A2->IE &= ~BIT3;
+	
+	
+}	
 
 BYTE uart0_getchar()
 {
 	BYTE inChar;
 	// Wait for data
 	// IFG register
-
+	while (!(EUSCI_A0->IFG & BIT0)) {}
+	
 
 	// read character and store in inChar variable
 	// RXBUF register
-	while((EUSCI_A0->IFG & BIT0) == 0 );
-	
 	inChar = EUSCI_A0->RXBUF;
 	
 
-	//Return the 8-bit data from the receiver
-	return(inChar);	
+	//Return the 8-bit data from the receiver 
+	return(inChar);
 
+}
+
+BYTE uart2_getchar()
+{
+	BYTE inChar;
+	// Wait for data
+	// IFG register
+	while (!(EUSCI_A2->IFG & BIT0)) {}
+	
+
+	// read character and store in inChar variable
+	// RXBUF register
+	inChar = EUSCI_A2->RXBUF;
+	
+	//Return the 8-bit data from the receiver 
+	return(inChar);
+
+}
+
+
+BOOLEAN uart0_dataAvailable()
+{
+		BOOLEAN go = FALSE;
+		if (EUSCI_A0->IFG & BIT0) 
+		{
+			go = TRUE;
+		}
+		return go;
+}
+
+BOOLEAN uart2_dataAvailable()
+{
+		BOOLEAN go = FALSE;
+		if (EUSCI_A2->IFG & BIT0) 
+		{
+			go = TRUE;
+		}
+		return go;
 }
 
 void uart0_putchar(char ch)
 {
-
 	// Wait until transmission of previous bit is complete 
 	// IFG register
-	while ((EUSCI_A0->IFG & BIT1) == 0);
+	//EUSCI_A0->IE |= BIT1;
+	while (!(EUSCI_A0->IFG & BIT1)) {}
 	
 	// send ch character to uart
 	// TXBUF register 
 	EUSCI_A0->TXBUF = ch;
-	//UCA0TXBUF = ch;
-	
 
+}
+
+void uart2_putchar(char ch)
+{
+	// Wait until transmission of previous bit is complete 
+	// IFG register
+	//EUSCI_A0->IE |= BIT1;
+	while (!(EUSCI_A2->IFG & BIT1)) {}
+	
+	// send ch character to uart
+	// TXBUF register 
+	EUSCI_A2->TXBUF = ch;
 
 }
 
@@ -114,5 +197,11 @@ void uart0_put(char *ptr_str)
 {
 	while(*ptr_str != 0)
 		uart0_putchar(*ptr_str++);
+}
+
+void uart2_put(char *ptr_str)
+{
+	while(*ptr_str != 0)
+		uart2_putchar(*ptr_str++);
 }
 
